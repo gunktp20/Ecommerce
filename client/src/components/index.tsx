260597530c3f28dfa9b18e159cc0123506cc0d08
gunktp14 <arrliver@gmail.com>
2024-02-11 13:41:02 +0700

    feat: landing-page

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./Navbar";
 import Card from "./Card"
 import SimpleSlider from "./SimpleSlider";
+import Footer from "./Footer";
 
 
-export { Navbar, Card, SimpleSlider }
+export { Navbar, Card, SimpleSlider , Footer}
